a7a2c80bfbc425247b769cf7316c742eec8cbbfe
nicolasdev97 <nicolas.po.im@gmail.com>
2026-05-21 13:47:10 -0500

    Exercise 13.15.

--- part13/project/controllers/blogs.js
+++ part13/project/controllers/blogs.js
@@ -36,7 +36,8 @@ router.get('/', async (req, res) => {
             model: User,
             attributes: ['username', 'name']
         },
-        where
+        where,
+        order: [['likes', 'DESC']]
     })
 
     res.json(blogs)
